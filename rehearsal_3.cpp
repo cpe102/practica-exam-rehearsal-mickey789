711aec83cbc8e76050116b097f10a4a1a21e7640
lab314-1 <lab314-1@labeng.local>
2020-01-10 16:09:49 +0700

    v2

--- rehearsal_3.cpp
+++ rehearsal_3.cpp
@@ -22,8 +22,8 @@ int main()
     double b = sumSqrt(10);
     double c = sumSqrt(5);
     double d = sumSqrt(2);
-    double e = sumSqrt(1);
-    double f = sumSqrt(0);
+    double e = sumSqrt(3);
+    double f = sumSqrt(-5);
     double g = sumSqrt(-1);
     
     cout << a << "\n" << b << "\n" << c << "\n" << d << "\n" << e << "\n" << f << "\n" << g << "\n";
