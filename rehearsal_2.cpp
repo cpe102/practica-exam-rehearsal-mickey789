a79ed0449f3e7c0563622949fbc3a4b0f875aed0
lab314-1 <lab314-1@labeng.local>
2020-01-10 15:32:58 +0700

    v1

--- rehearsal_2.cpp
+++ rehearsal_2.cpp
@@ -1,1 +1,25 @@
+#include<iostream>
+using namespace std;
+
+
+float sum = 0;
+float x;
+int main(){
+    cout << "Enter x: ";
+    cin >> x;
+    while(x != 0){
+        if(x > 0){
+            sum += x;
+        }else{
+            ;
+        }
+        cout << "Enter x: ";
+        cin >> x;
+    }
+    cout << "sum = " <<sum;
+    return 0;
+
+}
+
+
 
