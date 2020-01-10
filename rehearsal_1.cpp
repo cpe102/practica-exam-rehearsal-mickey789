24006be8e5c353c01bc36336de0c78455ec3a7d5
lab314-1 <lab314-1@labeng.local>
2020-01-10 15:21:33 +0700

    v1

--- rehearsal_1.cpp
+++ rehearsal_1.cpp
@@ -1,1 +1,23 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+
+string N;
+float G;
+
+int main(){
+    cout << "What is your name?: ";
+    cin >> N;
+    cout << "What is your GPA?: ";
+    cin >> G;
+    if(G >= 3.50){
+        cout << N << " InW Jrim Jrim!!!";
+    }else{
+        cout << "Try harder, " << N <<"!!!";
+    }
+    return 0;
+
+}
+
 
